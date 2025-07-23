ff644fbcc7d6cf930935087a35a94dfa198edbcb
Jack <jackhill9090@gmail.com>
2025-07-23 21:22:22 +0100

    finished part one, started part2

--- 2022/01/part1.c
+++ 2022/01/part1.c
@@ -3,14 +3,39 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
-  char *file = read_file("test.txt");
+  char *file = read_file("../2022/01/test.txt");
   char **fileList = splitOn(file, '\n');
+  free(file);
+
+  int highest[3] = {0, 0, 0};
+  int current_total = 0;
+
+  for (int i = 0; fileList[i] != NULL; i++) {
+    if (strcmp(fileList[i], "") != 0) {
+      current_total += atoi(fileList[i]);
+    } else {
+      for (int j = 0; j < 3; j++) {
+        if (highest[j] < current_total) {
+          highest[j] = current_total;
+          break;
+        }
+      }
+
+      current_total = 0;
+    }
+    free(fileList[i]);
+  }
   free(fileList);
-  // free(file);
+  int sum = 0;
+  for (int i = 0; i < 3; i++) {
 
-  // print_string(fileList);
+    printf("%d\n", highest[i]);
+    sum += highest[i];
+  }
 
+  printf("%d\n", sum);
   return 0;
 }
